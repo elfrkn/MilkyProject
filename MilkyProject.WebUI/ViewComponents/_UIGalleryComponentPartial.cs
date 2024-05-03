b2627e8952e9e58005334e36f2333fb492e3b7f9
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-05-03 20:21:00 +0200

    Complate ViewComponents

---
+++ MilkyProject.WebUI/ViewComponents/_UIGalleryComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkyProject.WebUI.ViewComponents
+{
+    public class _UIGalleryComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    
+}
---
+++ MilkyProject.WebUI/ViewComponents/_UIServiceComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MilkyProject.WebUI.ViewComponents
+{
+    public class _UIServiceComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
