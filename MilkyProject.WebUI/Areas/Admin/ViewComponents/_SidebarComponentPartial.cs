2b9c90b6b79b3b70883ace45507578a8992bb643
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-05-09 15:24:07 +0200

    AdminLayout

---
+++ MilkyProject.WebUI/Areas/Admin/ViewComponents/_SidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkyProject.WebUI.Areas.Admin.ViewComponents
+{
+    public class _SidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
